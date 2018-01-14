7eaa1cc35ba485a99a8c6c344f13b781637902b0
bilkitg <bilkitg@lightnin.hopkins>
2018-01-14 14:22:37 -0800

    A script that tests whether tensorflow was installed with compatible dependencies. According to the latest documentation (late '17), tensorflow's latest version was compatible only with Cuda 8.0. Meanwhile, NVIDIA's docs provided instructions mainly for downloading Cuda 9.0. Please see personal docs for info about how to download Cuda 8.0.

---
+++ TestTensorflowImport/TestTensorflowImport.py
@@ -0,0 +1,14 @@
+#!/usr/bin
+import math
+
+def main():
+	print("Hello world")
+	print("\n")
+	print("Testing math module\n|2| = " + str(math.fabs(2)))
+	print("\n")
+	print("Attempt to import tensorflow...")
+	import tensorflow
+	print("Imported! :D")
+
+if (__name__ == "__main__"):
+	main()
