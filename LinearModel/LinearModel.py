6846ebaa3c01e09529c399aeb3af5a655ba7148b
bilkitg <lg___gl@outlook.com>
2018-01-27 21:41:20 -0800

    Adding linear model project

---
+++ LinearModel/LinearModel.py
@@ -0,0 +1,27 @@
+#!/usr/bin
+import tensorflow as tf
+# 
+# Linear model
+#
+
+# create one -> generates, initializes and returns a linear model
+# train one -> takes a linear model, cost function, and some data and optimizes its parameters
+# get data -> loads some data into a structure and returns it
+#					 
+
+def CreateLinearModel(weight, bias):
+	print("--->Creating linear model")
+	return None
+
+def TrainLinearModel(linearModel, costFunc, data):
+	print("--->Training linear model")
+	return None
+
+def main():
+	print("<<< Start main >>>\n")
+	CreateLinearModel(1, 1)
+	
+	print("<<< End main >>>\n")
+	
+if (__name__ == "__main__"):
+	main()
