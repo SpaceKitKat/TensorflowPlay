bf52cfde77ed7c2c8bbfad11a0d6a3589ec1d0c8
bilkitg <lg___gl@outlook.com>
2018-01-27 22:04:33 -0800

    Added basic class that represents a linear model.

--- LinearModel/LinearModel.py
+++ LinearModel/LinearModel.py
@@ -7,20 +7,42 @@ import tensorflow as tf
 # create one -> generates, initializes and returns a linear model
 # train one -> takes a linear model, cost function, and some data and optimizes its parameters
 # get data -> loads some data into a structure and returns it
-#					 
+# run one -> takes model and inputs then prints outputs					 
 
-def CreateLinearModel(weight, bias):
-	print("--->Creating linear model")
-	return None
+c_FLOAT_TYPE = tf.float32
+Session = tf.Session()
 
-def TrainLinearModel(linearModel, costFunc, data):
-	print("--->Training linear model")
-	return None
+class LinearModel:
+	w = None
+	x = None
+	b = None
+	def __init__(self, weight, bias):
+		print("--->Creating linear model")
+		# Use float type to conserve memory
+		self.w = tf.Variable([weight * 1.0], dtype=tf.float32)
+		self.b = tf.Variable([bias * 1.0], dtype=tf.float32)
+		self.x = tf.placeholder(dtype=tf.float32)
+
+		# Initialize variables
+		Session.run(tf.global_variables_initializer())
+		print("--->Created and initialized model.")
+
+	def TrainLinearModel(self, costFunc, data):
+		print("--->Training linear model")
+		return None
+
+	def RunLinearModel(self, arrInputs):
+		print("--->Running linear model")
+		linearModel = self.w * self.x + self.b
+		print(Session.run(linearModel, { self.x: arrInputs }))
+		return None
 
 def main():
 	print("<<< Start main >>>\n")
-	CreateLinearModel(1, 1)
 	
+	identityLinearModel = LinearModel(1, 0)
+	identityLinearModel.RunLinearModel([0, 2, 3, 5, 7])
+
 	print("<<< End main >>>\n")
 	
 if (__name__ == "__main__"):
